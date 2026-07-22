53b9f5ece06de4a2a4cdfa62c9772b7c9ace1b02
Long <le7929590@gmail.com>
2026-07-22 11:31:28 +0700

    sdfsdfsdf

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -40,6 +40,6 @@ export const formatDateLast = (dateStr) => {
 export const getYesterday = () => {
     const today = new Date();
     const yesterday = new Date(today);
-    yesterday.setDate(today.getDate() - 1);
+    yesterday.setDate(today.getDate() - 22); //1
     return yesterday.toISOString().split('T')[0];
 };
